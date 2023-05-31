5a9639b61fe51da47b623075500e1ce2388e08ff
algar67 <122879171+algar67@users.noreply.github.com>
2023-05-31 20:05:43 +0200

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello World´s ALGAR',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
